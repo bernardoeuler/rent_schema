96971129184a5629483ed5b7edb61edc62e0a81d
Bernardo Euler <bernardoeulersantos@gmail.com>
2026-01-10 23:17:20 -0300

    fix: Correct relative imports in tests

--- app/teste/models/user_model/get-user-by-cpf.py
+++ app/teste/models/user_model/get-user-by-cpf.py
@@ -1,4 +1,4 @@
-from ...models.user_model import UserModel
+from ....models.user_model import UserModel
 
 UserModel().create_user("12345678900", "Bernardo Euler", "12345678912345678900", "12345678900", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3")
 
